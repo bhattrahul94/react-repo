7a6041c305b5449b45552d7717d4da32a5f07f47
bhattrahul94 <bhattrahul94@gmail.com>
2023-07-30 18:24:59 +0530

    Added Functions

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello World From React!!!")
+// const heading = React.createElement("h1", {}, "Hello World From React!!!")
+
+
+const Heading1 = () => <h1 id="heading1"> Inside Heading1 </h1>
+
+const number = 10000
+const Heading2 = () => (
+        <div id="container">
+            {number}
+            <h1 id="heading2"> Inside Heading2 😇 </h1>
+        </div>
+    )
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(<Heading2 />)
